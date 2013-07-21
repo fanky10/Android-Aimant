23dc9407b86ba6f7f394b4acd14dd0802c3973f9
Facundo Scoccia <fanky10@gmail.com>
2013-07-21 19:47:15 -0300

    clean-up

--- src/com/mawape/aimant/activities/NegociosListActivity.java
+++ src/com/mawape/aimant/activities/NegociosListActivity.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import com.mawape.aimant.R;
@@ -12,28 +11,29 @@ import com.mawape.aimant.entities.Negocio;
 import com.mawape.aimant.services.impl.NegociosManagerImpl;
 import com.mawape.aimant.widget.NegociosArrayAdapter;
 
-public class NegociosListActivity extends BaseActivity{
-	
+public class NegociosListActivity extends BaseActivity {
+
 	private static final String TAG = "NegociosActivity";
-	
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.negocios_list);
 		init();
 	}
-	
-	private void init(){
+
+	private void init() {
 		final List<Negocio> opcionV = getInforme();
-		Log.d(TAG,"el retorno fue: "+opcionV);
-		ListView lstOpciones = (ListView)findViewById(R.id.lstMenu);
-		lstOpciones.setAdapter(new NegociosArrayAdapter(getApplicationContext(), opcionV));
+		Log.d(TAG, "el retorno fue: " + opcionV);
+		ListView lstOpciones = (ListView) findViewById(R.id.lstMenu);
+		lstOpciones.setAdapter(new NegociosArrayAdapter(
+				getApplicationContext(), opcionV));
 	}
 
 	/**
 	 * la idea es sacar el informecin de la db :D
 	 */
-	protected List<Negocio> getInforme(){
+	protected List<Negocio> getInforme() {
 		return new NegociosManagerImpl(getApplicationContext()).getNegocios();
 	}
 }
