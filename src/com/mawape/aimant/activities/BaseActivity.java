47b9385648c7114a83c7fff6cc462aa832b6ce2c
Facundo Scoccia <fanky10@gmail.com>
2013-07-26 12:19:25 -0300

    orientacion portrait for all activities

--- src/com/mawape/aimant/activities/BaseActivity.java
+++ src/com/mawape/aimant/activities/BaseActivity.java
@@ -1,7 +1,13 @@
 package com.mawape.aimant.activities;
 
 import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
 
-public abstract class BaseActivity extends Activity{
-
+public abstract class BaseActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+	}
 }
