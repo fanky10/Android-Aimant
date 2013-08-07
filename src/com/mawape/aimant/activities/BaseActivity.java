049910f7adb4b48edf482c23bcbaa9959d684cd0
Facundo Scoccia <fanky10@gmail.com>
2013-08-07 12:43:51 -0300

    refactor menu -- re-usable menu-bar

--- src/com/mawape/aimant/activities/BaseActivity.java
+++ src/com/mawape/aimant/activities/BaseActivity.java
@@ -1,13 +1,55 @@
 package com.mawape.aimant.activities;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.text.Html;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
+
+import com.mawape.aimant.R;
 
 public abstract class BaseActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+	}
+
+	protected void configureMenuBar() {
+		// handling menu info click
+		ImageView infoImg = (ImageView) findViewById(R.id.commonMenuInformation);
+		infoImg.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				showDialog();
+			}
+		});
+	}
+
+	private void showDialog() {
+		// 1. Instantiate an AlertDialog.Builder with its constructor
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+		// 2. Chain together various setter methods to set the dialog
+		// characteristics
+		String title = getString(R.string.info_title);
+		String message = getString(R.string.info_message);
+		builder.setMessage(Html.fromHtml(message))
+				.setTitle(Html.fromHtml(title))
+				.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+					@Override
+					public void onClick(DialogInterface dialog, int id) {
+						// do nothing at all
+					}
+				});
+
+		// 3. Get the AlertDialog from create()
+		AlertDialog dialog = builder.create();
+		dialog.show();
 	}
 }
--- src/com/mawape/aimant/activities/CategoriasAcivity.java
+++ src/com/mawape/aimant/activities/CategoriasAcivity.java
@@ -2,13 +2,7 @@ package com.mawape.aimant.activities;
 
 import java.util.List;
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.os.Bundle;
-import android.text.Html;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.ImageView;
 import android.widget.ListView;
 
 import com.mawape.aimant.R;
@@ -23,6 +17,7 @@ public class CategoriasAcivity extends BaseActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.categorias_list);
 		init();
+		configureMenuBar();
 	}
 
 	private void init() {
@@ -31,38 +26,7 @@ public class CategoriasAcivity extends BaseActivity {
 				getApplicationContext(), opcionV);
 		ListView lstOpciones = (ListView) findViewById(R.id.catListMenu);
 		lstOpciones.setAdapter(adaptador);
-		lstOpciones.setOnItemClickListener(adaptador);
-		ImageView infoImg = (ImageView) findViewById(R.id.catMenuInformation);
-		infoImg.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				showDialog();
-			}
-		});
-
-	}
-
-	private void showDialog() {
-		// 1. Instantiate an AlertDialog.Builder with its constructor
-		AlertDialog.Builder builder = new AlertDialog.Builder(this);
-
-		// 2. Chain together various setter methods to set the dialog
-		// characteristics
-		String title = getString(R.string.info_title);
-		String message = getString(R.string.info_message);
-		builder.setMessage(Html.fromHtml(message))
-				.setTitle(Html.fromHtml(title))
-				.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-					@Override
-					public void onClick(DialogInterface dialog, int id) {
-						// do nothing at all
-					}
-				});
-
-		// 3. Get the AlertDialog from create()
-		AlertDialog dialog = builder.create();
-		dialog.show();
+		lstOpciones.setOnItemClickListener(adaptador);		
 	}
 
 	private List<Categoria> getCategorias() {
